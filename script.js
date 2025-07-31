3f2fa280ef4a934e4c25ca4f46e3b10dcf15c13c
Daniel Kolocka <kolockadaniel@hotmail.com>
2025-07-30 20:30:04 -0700

    add js to create book and append to display

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const library = document.querySelector(".library");
+
 const myLibrary = [];
 
 function Book(title, author, numPages, status) {
@@ -14,18 +16,43 @@ function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus) {
 }
 
 // console.log("Library: " + myLibrary);
-addBookToLibrary("Title1", "John Doe", 223, "Read");
-addBookToLibrary("Title2", "Steven Sarm", 105, "Not Read");
+addBookToLibrary("Title1", "John Doe", 223, true);
+addBookToLibrary("Title2", "Steven Sarm", 105, false);
 
 // console.log("Added a book. New Library: " + myLibrary[0].id);
 
 function printLibrary(libraryArray) {
     const length = libraryArray.length;
     for (let i=0; i<length; i++) {
-        console.log(`Title: ${libraryArray[i].title}, author: ${libraryArray[i].author}, number of pages: ${libraryArray[i].numPages}, status: ${libraryArray[i].status}, id: ${libraryArray[i].id}`);
+        // console.log(`Title: ${libraryArray[i].title}, author: ${libraryArray[i].author}, number of pages: ${libraryArray[i].numPages}, status: ${libraryArray[i].status}, id: ${libraryArray[i].id}`);
+        const newBook = document.createElement("div");
+        newBook.setAttribute("class", "book");
+
+        const newTitle = document.createElement("p");
+        newTitle.textContent = libraryArray[i].title;
+        const newAuthor = document.createElement("p");
+        newAuthor.textContent = libraryArray[i].author;
+        const newPages = document.createElement("p");
+        newPages.textContent = libraryArray[i].numPages + " Pages";
+
+        const statusButton = document.createElement("button");
+        statusButton.setAttribute("id", "changeStatus");
+        statusButton.textContent = libraryArray[i].status ? "Read" : "Not Read";
+        const deleteButton = document.createElement("button");
+        deleteButton.setAttribute("id", "deleteBook");
+        deleteButton.textContent = "Delete";
+
+        library.appendChild(newBook);
+        newBook.appendChild(newTitle);
+        newBook.appendChild(newAuthor);
+        newBook.appendChild(newPages);
+        newBook.appendChild(statusButton);
+        newBook.appendChild(deleteButton);
     }
     return;
 }
 
 printLibrary(myLibrary);
+
+
 
